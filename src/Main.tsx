f52b016972a76e8133afb8a9a7ce7fd5833b28d7
MateusPitura <mateuspitura@gmail.com>
2023-10-28 14:25:03 -0300

    feat: adicionado navegação com bottom tabs
    Criado estrutura de navegação com o bottom tabs

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import News from './screen/News';
+import Calendar from './screen/Calendar';
+import Bookmark from './screen/Bookmark';
+import Setting from './screen/Setting';
+
+const bottomTabNavigator = createBottomTabNavigator();
 
 export default function () {
     return (
-        <View>
-            <Text>Hello World</Text>
-        </View>
+        <NavigationContainer>
+            <bottomTabNavigator.Navigator>
+                <bottomTabNavigator.Screen
+                    name="News"
+                    component={News}
+                    options={{ title: "News" }}
+                />
+                <bottomTabNavigator.Screen
+                    name="Calendar"
+                    component={Calendar}
+                    options={{ title: "Calendar" }}
+                />
+                <bottomTabNavigator.Screen
+                    name="Bookmark"
+                    component={Bookmark}
+                    options={{ title: "Bookmark" }}
+                />
+                <bottomTabNavigator.Screen
+                    name="Setting"
+                    component={Setting}
+                    options={{ title: "Setting" }}
+                />
+            </bottomTabNavigator.Navigator>
+        </NavigationContainer>
     );
 }; 
