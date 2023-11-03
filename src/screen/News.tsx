e2049191238d84763e7f0e4fbd333bdf2ef28ce0
MateusPitura <mateuspitura@gmail.com>
2023-11-03 14:15:14 -0300

    feat: criado lista de notícias

--- src/screen/News.tsx
+++ src/screen/News.tsx
@@ -1,10 +1,107 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
-export default function() {
+//style
+import StyleAuxiliar from '../style/ScreenNews'
+import Typography from '../asset/design/Typography';
+
+export default function () {
+
+    const Style = StyleAuxiliar();
+
     return (
-        <View >
-            <Text>Tela News</Text>
+        <View style={[Style.container]}>
+            <View style={Style.listContainer}>
+                <View style={Style.imageContainer}>
+                    <Image
+                        source={require("../asset/image/image.webp")}
+                        style={Style.image}
+                    />
+                </View>
+                <View style={Style.text}>
+                    <Text
+                        numberOfLines={1}
+                        style={[Typography.titleMedium, Style.headlineText]}
+                    >
+                        Lorem ipsum dolor sit amet.
+                    </Text>
+                    <Text
+                        numberOfLines={2}
+                        style={[Typography.bodyMedium, Style.supportText]}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quia ratione quibusdam dolores sint quasi ab, pariatur veritatis eum cumque.
+                    </Text>
+                </View>
+                <View style={Style.date}>
+                    <Text
+                        numberOfLines={2}
+                        style={[Typography.labelSmall, Style.trailingText]}
+                    >
+                        10+
+                    </Text>
+                </View>
+            </View>
+            <View style={Style.listContainer}>
+                <View style={Style.imageContainer}>
+                    <Image
+                        source={require("../asset/image/image.webp")}
+                        style={Style.image}
+                    />
+                </View>
+                <View style={Style.text}>
+                    <Text
+                        numberOfLines={1}
+                        style={[Typography.titleMedium, Style.headlineText]}
+                    >
+                        Lorem ipsum dolor sit amet.
+                    </Text>
+                    <Text
+                        numberOfLines={2}
+                        style={[Typography.bodyMedium, Style.supportText]}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quia ratione quibusdam dolores sint quasi ab, pariatur veritatis eum cumque.
+                    </Text>
+                </View>
+                <View style={Style.date}>
+                    <Text
+                        numberOfLines={2}
+                        style={[Typography.labelSmall, Style.trailingText]}
+                    >
+                        10+
+                    </Text>
+                </View>
+            </View>
+            <View style={Style.listContainer}>
+                <View style={Style.imageContainer}>
+                    <Image
+                        source={require("../asset/image/image.webp")}
+                        style={Style.image}
+                    />
+                </View>
+                <View style={Style.text}>
+                    <Text
+                        numberOfLines={1}
+                        style={[Typography.titleMedium, Style.headlineText]}
+                    >
+                        Lorem ipsum dolor sit amet.
+                    </Text>
+                    <Text
+                        numberOfLines={2}
+                        style={[Typography.bodyMedium, Style.supportText]}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quia ratione quibusdam dolores sint quasi ab, pariatur veritatis eum cumque.
+                    </Text>
+                </View>
+                <View style={Style.date}>
+                    <Text
+                        numberOfLines={2}
+                        style={[Typography.labelSmall, Style.trailingText]}
+                    >
+                        8 May
+                    </Text>
+                </View>
+            </View>
         </View>
+
     )
 } 
--- src/style/ScreenNews.tsx
+++ src/style/ScreenNews.tsx
@@ -10,6 +10,45 @@ export default function(){
     return StyleSheet.create({
         container:{
             backgroundColor: Color.surface,
+            flex: 1,
+            paddingHorizontal: 16,
+            paddingVertical: 8,
+        },
+        listContainer:{
+            flexDirection: 'row',
+            height: 88,
+            paddingVertical: 12,
+            paddingHorizontal: 16,
+            borderRadius: 5,
+        },
+        imageContainer:{
+            paddingRight: 16,
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+        },
+        image:{
+            width: 56,
+            height: 56,
+            borderRadius: 5,
+        },
+        text:{
+            flex: 5,
+            justifyContent: 'flex-start',
+            paddingRight: 16,
+        },
+        headlineText:{
+            color: Color.onSurface,
+        },
+        supportText:{
+            color: Color.onSurfaceVariant,
+        },
+        date:{
+            alignItems: 'flex-end',
+            flex: 1,
+            
+        },
+        trailingText:{
+            color: Color.onSurfaceVariant,
         }
     })
 }
